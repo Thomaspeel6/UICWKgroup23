f84f56c596a01a72c8e8ee7b5cd0f313835dc1f0
Igor-Kaminski <91342396+Igor-Kaminski@users.noreply.github.com>
2024-12-14 15:59:06 +0000

    iteration 2 fluorinated changes

--- FluorinatedCompoundsPage.hpp
+++ FluorinatedCompoundsPage.hpp
@@ -1,17 +1,35 @@
-#ifndef FLUORINATED_COMPOUNDS_PAGE_HPP
-#define FLUORINATED_COMPOUNDS_PAGE_HPP
+#pragma once
 
 #include <QWidget>
+#include <QtCharts>
+#include "model.hpp"
 
-class FluorinatedCompoundsPage : public QWidget
-{
-    Q_OBJECT
+
+class QChartView;
+class QPushButton;
+class QComboBox;
+
+
+class FluorinatedCompoundsPage : public QWidget {
+
 
 public:
     explicit FluorinatedCompoundsPage(QWidget* parent = nullptr);
 
 private:
+    void setupUI();
+    void setupChart();
+    void connectSignals();
 
+    QChartView* chartView;
+    QComboBox* pollutantSelector;
+    QPushButton* backButton;
+    waterQualityModel* model;
+
+
+signals:
+    void returnToDashboard();
+
+public slots:
+    void updateChart(const QString& pollutantName);
 };
-
-#endif 
